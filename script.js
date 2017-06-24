e885b0097bdcd0dc8f6fdf0021d4d8b6185e07ab
Lorenz Schwittmann <schwittmann@vs.uni-due.de>
2017-06-24 21:02:33 +0200

    test

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ function deleteStuff(t) {
 	})
 };
 
+
 TrelloPowerUp.initialize({
 	'card-buttons': function(t, options){
 		return [{
 			icon: './images/icon.svg',
-			text: 'Delete checked items',
+			text: 'Delete checked',
 			callback: deleteStuff}];
-}
+	},
+	'authorization-status' : function(t) {
+		return new TrelloPowerUp.Promise( function(resolve) {
+			t.get('board', 'private', 'deleteToken', null).then(function(v) {
+				resolve({ authorized: v != null });
+			})
+		})
+	},
+	'show-authorization' : function (t) {
+		 Trello.authorize({
+		 	type:"popup",
+		 	name:"Delete Checked Items Button",
+		 	interactive:false,
+		 	expiration: "never",
+		 	success:function() {
+		 		t.set('board', 'private', 'deleteToken', localStorage["trello_token"]);
+		 	},
+		 	scope: {read: "allowRead", write: "allowWrite"}
+		 });
+	}
 });
